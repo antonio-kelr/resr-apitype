03dceea6e75a2f448df4d7ae811dc0d65779a577
Clenilson Lopes <lorimweb@gmail.com>
2024-06-06 01:13:31 -0300

    ajustando nomes de metodos

--- src/server/controllers/cidades/DeleteById.ts
+++ src/server/controllers/cidades/DeleteById.ts
@@ -15,7 +15,7 @@ export const DeleteValidation = validation((getSchema) => ({
   })),
 }));
 
-export const deliteBayID = async (req: Request<IQueryProps>, res: Response) => {
+export const deleteById = async (req: Request<IQueryProps>, res: Response) => {
   if (!req.params.id) {
     return res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -12,7 +12,7 @@ router.get('/cidades',TokemValidation, criateCidade.GetAllValidation, criateCida
 router.post('/cidades',TokemValidation, criateCidade.CreateValidation, criateCidade.Create);
 router.get('/cidades/:id',TokemValidation, criateCidade.GetByIdValidation, criateCidade.getById);
 router.put('/cidades/:id',TokemValidation, criateCidade.updateValidation, criateCidade.update);
-router.delete('/cidades/:id',TokemValidation, criateCidade.DeleteValidation, criateCidade.deliteBayID);
+router.delete('/cidades/:id',TokemValidation, criateCidade.DeleteValidation, criateCidade.deleteById);
 
 
 
@@ -20,13 +20,13 @@ router.get('/pessoas',TokemValidation, criatePesoa.GetAllValidation, criatePesoa
 router.post('/pessoas',TokemValidation, criatePesoa.CreateValidation, criatePesoa.Create);
 router.get('/pessoas/:id',TokemValidation, criatePesoa.GetByIdValidation, criatePesoa.getById);
 router.put('/pessoas/:id',TokemValidation, criatePesoa.updateValidation, criatePesoa.update);
-router.delete('/pessoas/:id',TokemValidation, criatePesoa.DeleteValidation, criatePesoa.deliteBayID);
+router.delete('/pessoas/:id',TokemValidation, criatePesoa.DeleteValidation, criatePesoa.deleteById);
 
 router.get('/agendas',TokemValidation, criateAgenda.GetAllValidation, criateAgenda.getAll);
 router.post('/agendas',TokemValidation, criateAgenda.CreateValidation, criateAgenda.Create);
 router.get('/agendas/:id',TokemValidation, criateAgenda.GetByIdAgenda, criateAgenda.getById);
 router.put('/agendas/:id',TokemValidation, criateAgenda.updateAgenda, criateAgenda.update);
-router.delete('/agendas/:id',TokemValidation, criateAgenda.DeleteValidation, criateAgenda.deliteBayID);
+router.delete('/agendas/:id',TokemValidation, criateAgenda.DeleteValidation, criateAgenda.deleteById);
 
 
 
